be13093c83374dd28faf727ceaaa63db5eb01306
Daniel Febrian <danielfebrian61@gmail.com>
2023-10-19 09:50:00 +0700

    added list date page

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="style2.css" rel="stylesheet">
+    <link rel="stylesheet" href="style2.css">
     <title>Login</title>
 </head>
 
